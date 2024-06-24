85a7ed298d2c189d393b5b452e9c47497f0a0391
alexisabels <isabelsanchezalex@gmail.com>
2024-06-24 20:09:34 +0200

    api call recipes

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,31 @@
 import { Search } from "lucide-react";
-import React from "react";
 
+import RecipeCard from "../components/RecipeCard";
+import { useEffect, useState } from "react";
+import { getRandomColor } from "../lib/utils";
+const APP_ID = "02972313";
+const APP_KEY = "4d20fae25f6650c77860168a47abe756";
 const Homepage = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const fetchRecipes = async (searchQuery) => {
+    setLoading(true);
+    setRecipes([]);
+    try {
+      const res = await fetch(
+        `https://api.edamam.com/api/recipes/v2/?app_id=${APP_ID}&app_key=${APP_KEY}&q=${searchQuery}&type=public`
+      );
+      const data = await res.json();
+      setRecipes(data.hits);
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchRecipes("chicken");
+  }, []);
   return (
     <>
       <div className="bg-[#faf9fb] p-10 flex-1">
@@ -23,7 +47,23 @@ const Homepage = () => {
           <p className="text-slate-500 font-semibold ml-1 my-2 text-sm tracking-tight">
             Opciones populares
           </p>
-          <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3"></div>
+          <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+            {!loading &&
+              recipes.map(({ recipe }, index) => (
+                <RecipeCard key={index} recipe={recipe} {...getRandomColor()} />
+              ))}
+            {loading &&
+              [...Array(9)].map((_, index) => (
+                <div key={index} className="flex flex-col gap-4 w-full">
+                  <div className="skeleton h-32 w-full"></div>
+                  <div className="flex justify-between">
+                    <div className="skeleton h-4 w-28"></div>
+                    <div className="skeleton h-4 w-24"></div>
+                  </div>
+                  <div className="skeleton h-4 w-1/2"></div>
+                </div>
+              ))}
+          </div>
         </div>
       </div>
     </>
