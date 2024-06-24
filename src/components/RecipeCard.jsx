69b623bae3ca8681905aafb7c058cacf980e4ab4
alexisabels <isabelsanchezalex@gmail.com>
2024-06-24 20:09:56 +0200

    recipe card api

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,19 +1,29 @@
 import { Heart, HeartPulse, Soup } from "lucide-react";
 import React from "react";
 
-function RecipeCard() {
+function RecipeCard({ recipe, bg, badge }) {
+  const getTwoValuesFromArray = (arr) => {
+    return [arr[0], arr[1]];
+  };
+  const healthLabels = getTwoValuesFromArray(recipe.healthLabels);
+
   return (
     <>
-      {" "}
-      <div className="flex flex-col rounded-md bg-[#ecf7d4] overflow-hidden p-3 relative">
-        <a href="#" className="relative h-32">
+      <div
+        className={`flex flex-col rounded-md ${bg} overflow-hidden p-3 relative`}
+      >
+        <a
+          href={`https://youtube.com/results?search_query=${recipe.label} receta`}
+          target="_blank"
+          className="relative h-32"
+        >
           <img
-            src="/1.jpg"
+            src={recipe.image}
             alt="recipe img"
             className="rounded-md w-full h-full  object-cover cursor-pointer"
           />
           <div className="absolute bottom-2 left-2 bg-white rounded-full p-1 cursor-pointer flex items-center gap-1 text-sm">
-            <Soup size={24} /> 4 servings
+            <Soup size={24} /> {recipe.yield} Servings
           </div>
           <div className="absolute top-1 right-2 bg-white rounded-full p-1 cursor-pointer">
             <Heart
@@ -23,22 +33,21 @@ function RecipeCard() {
           </div>
         </a>
         <div className="flex mt-1">
-          <p className="font-bold tracking-wide">Roasted chicken</p>
+          <p className="font-bold tracking-wide">{recipe.label}</p>
         </div>
-        <p className="my-2">Turkish chicken</p>
+        <p className="my-2 capitalize">{recipe.cuisineType}</p>
         <div className="flex gap-2 mt-auto">
-          <div className="flex gap-1 bg-[#d6f497] items-center p-1 rounded-md">
-            <HeartPulse size={16} />{" "}
-            <span className="text-sm tracking-tighter font-semibold">
-              Gluten free
-            </span>
-          </div>
-          <div className="flex gap-1 bg-[#d6f497] items-center p-1 rounded-md">
-            <HeartPulse size={16} />{" "}
-            <span className="text-sm tracking-tighter font-semibold">
-              Heart-healthy
-            </span>
-          </div>{" "}
+          {healthLabels.map((label, index) => (
+            <div
+              key={index}
+              className={`flex gap-1 ${badge} items-center p-1 rounded-md`}
+            >
+              <HeartPulse size={16} />{" "}
+              <span className="text-sm tracking-tighter font-semibold">
+                {label}
+              </span>
+            </div>
+          ))}
         </div>
       </div>
     </>
